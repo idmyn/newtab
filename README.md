eedde65036c9382a2267aa5f035ac5d2a59d2dd1
David Mynors <dmynors@gmail.com>
2018-10-27 11:09:05 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# newtab
-my personal new tab page
+# newtab - clean and minimal!
+- launches sites on keypress (currently quite messy duplication of html links and javascript)
+- horizontal center implementation also quite messy atm
---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener('keydown', function(event) {
+  if(event.keyCode == 67) {
+    window.location.href = 'https://lichess.org';
+  }
+  if(event.keyCode == 69) {
+    window.location.href = 'https://mail.tutanota.com';
+  }
+  if(event.keyCode == 76) {
+    window.location.href = 'http://learn.ed.ac.uk';
+  }
+  if(event.keyCode == 77) {
+    window.location.href = 'https://messenger.com';
+  }
+  if(event.keyCode == 80) {
+    window.location.href = 'https://play.pokemonshowdown.com';
+  }
+  if(event.keyCode == 82) {
+    window.location.href = 'http:/resourcelists.ed.ac.uk';
+  }
+  if(event.keyCode == 83) {
+    window.location.href = 'https://social.coop';
+  }
+  if(event.keyCode == 89) {
+    window.location.href = 'https://youtube.com';
+  }
+})
